c1a3322975adc1efc69587ba95ccf89317e8b7c2
Maciej Sołoducha <biuro@goldenapp.pl>
2025-07-01 12:37:51 +0200

    Change preferred Color Scheme

--- Werset z Biblii/View/Subview/SettingView.swift
+++ Werset z Biblii/View/Subview/SettingView.swift
@@ -12,77 +12,78 @@ struct SettingView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        NavigationStack {
-            VStack() {
-                
-                // Section theme
-                HStack {
-                    // Theme setting label
-                    Text("Wygląd")
-                        .font(.title3)
-                        .foregroundStyle(Color.accentColor)
-                        .bold()
+        ZStack {
+            NavigationStack {
+                VStack() {
+                    
+                    // Section theme
+                    HStack {
+                        // Theme setting label
+                        Text("Wygląd")
+                            .font(.title3)
+                            .foregroundStyle(Color.accentColor)
+                            .bold()
+                        
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 10)
+                    
+                    // Theme setting button
+                    HStack {
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            Image(systemName: "paintpalette")
+                                .font(.title2)
+                                .foregroundStyle(.black)
+                            Text("Wybór motywu")
+                                .font(.headline)
+                                .foregroundStyle(.black)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
+                        
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 5)
+                    
+                    // Section reminders
+                    HStack {
+                        // Reminders setting label
+                        Text("Powiadomienia")
+                            .font(.title3)
+                            .foregroundStyle(Color.accentColor)
+                            .bold()
+                        
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 20)
+                    
+                    // Reminders setting button
+                    HStack {
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            Image(systemName: "bell")
+                                .font(.title2)
+                                .foregroundStyle(.black)
+                            Text("Codzienne powiadomienie")
+                                .font(.headline)
+                                .foregroundStyle(.black)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 5)
                     
                     Spacer()
                 }
-                .padding(.horizontal, 20)
-                .padding(.top, 10)
-                
-                // Theme setting button
-                HStack {
-                    NavigationLink {
-                        EmptyView()
-                    } label: {
-                        Image(systemName: "paintpalette")
-                            .font(.title2)
-                            .foregroundStyle(.black)
-                        Text("Wybór motywu")
-                            .font(.headline)
-                            .foregroundStyle(.black)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                    }
-                    
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 5)
-                
-                // Section reminders
-                HStack {
-                    // Reminders setting label
-                    Text("Powiadomienia")
-                        .font(.title3)
-                        .foregroundStyle(Color.accentColor)
-                        .bold()
-                    
-                    Spacer()
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 20)
-                
-                // Reminders setting button
-                HStack {
-                    NavigationLink {
-                        EmptyView()
-                    } label: {
-                        Image(systemName: "bell")
-                            .font(.title2)
-                            .foregroundStyle(.black)
-                        Text("Codzienne powiadomienie")
-                            .font(.headline)
-                            .foregroundStyle(.black)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                    }
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 5)
-                
-                Spacer()
-            }
-            // Screen title
+                // Screen title
                 .navigationTitle("Ustawienia")
-            // Toolbar button exit from screen
+                // Toolbar button exit from screen
                 .toolbar {
                     ToolbarItem(placement: .topBarLeading) {
                         Image(systemName: "xmark")
@@ -91,6 +92,7 @@ struct SettingView: View {
                             }
                     }
                 }
+            }
         }
     }
 }
--- Werset z Biblii/Werset_z_BibliiApp.swift
+++ Werset z Biblii/Werset_z_BibliiApp.swift
@@ -14,6 +14,7 @@ struct Werset_z_BibliiApp: App {
         
         WindowGroup {
             HomeView()
+                .preferredColorScheme(.light)
                 .onAppear {
                     AudioManager.shared.playBackgroundMusic()
                 }
