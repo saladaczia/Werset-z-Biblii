9ecf89d2596462b8ce778f81724a8be0e4d4920b
Maciej Sołoducha <saladaczia@icloud.com>
2025-08-02 20:24:11 +0200

    Change start date.

--- Werset z Biblii/ViewModel/VersesViewModel.swift
+++ Werset z Biblii/ViewModel/VersesViewModel.swift
@@ -76,7 +76,7 @@ class VersesViewModel: ObservableObject {
         guard !verses.isEmpty else { return }
 
         let calendar = Calendar.current
-        let startDate = calendar.date(from: DateComponents(year: 2025, month: 7, day: 20))!
+        let startDate = calendar.date(from: DateComponents(year: 2025, month: 7, day: 14))!
         let today = calendar.startOfDay(for: Date())
 
         let daysPassed = calendar.dateComponents([.day], from: startDate, to: today).day ?? 0
